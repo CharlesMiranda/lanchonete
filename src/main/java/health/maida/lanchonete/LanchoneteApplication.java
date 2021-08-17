2d43c982f32b38fa38dc78c19679315312b38245
“Charles <charlesmiranda@inforgeneses.com.br>
2021-08-17 01:53:07 -0300

    feat: adicionando estrutura inicial do spring boot

---
+++ src/main/java/health/maida/lanchonete/LanchoneteApplication.java
@@ -0,0 +1,22 @@
+package health.maida.lanchonete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@RestController
+public class LanchoneteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanchoneteApplication.class, args);
+	}
+	
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+
+}
